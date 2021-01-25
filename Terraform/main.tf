bebdf0f96a1afda07bb2762a5f2367ae59fefc32
Ubuntu <ubuntu@ip-172-31-4-91.eu-west-3.compute.internal>
2021-01-25 13:58:25 +0000

    ssh key

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -22,6 +22,7 @@ resource "aws_instance" "instance_jenkins_server_karachira" {
   count         = var.create_instance ? var.instance_number : 0
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
+  key_name = var.ssh_key
 
   tags = {
     Name = var.instance_name
